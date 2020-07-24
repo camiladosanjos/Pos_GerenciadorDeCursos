3f63f82a1d48132f0bef24fb49fa095ee084bae4
Camila Vargas <camila.araujodosanjos@gmail.com>
2020-07-24 20:37:41 -0300

    commit

---
+++ Cursos.Domain/Entities/DetalhamentoCurso.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cursos.Domain.Entities
+{
+    public class DetalhamentoCurso
+    {
+        public Guid CursoId { get; set; }
+        public Curso Curso { get; set; }
+
+        public Guid DisciplinaId { get; set; }
+        public Disciplina Disciplina { get; set; }
+    }
+}
