6950ec778d034ea2b2aa2fe04cfc7430925caad1
Camila Vargas <camila.araujodosanjos@gmail.com>
2020-07-24 19:24:37 -0300

    Add project files.

---
+++ GerenciadorDeCursosPosInfnet/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GerenciadorDeCursosPosInfnet.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
