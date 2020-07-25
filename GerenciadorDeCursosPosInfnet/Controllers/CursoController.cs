6d96cd0433d32a6e6786d725c5eb619c495007ca
Camila Vargas <camila.araujodosanjos@gmail.com>
2020-07-24 23:29:39 -0300

    commit

---
+++ GerenciadorDeCursosPosInfnet/Controllers/CursoController.cs
@@ -0,0 +1,53 @@
+﻿using Cursos.Application.Intefaces;
+using Cursos.Application.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace Cursos.Presentation.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClienteController : ControllerBase
+    {
+        private readonly ICursoApplication _cursoApplication;
+        public ClienteController(ICursoApplication cursoApplication)
+        {
+            _cursoApplication = cursoApplication;
+        }
+        // GET: api/<Cliente>
+        [HttpGet]
+        public IEnumerable<CursoViewModel> Get()
+        {
+            return _cursoApplication.List();
+        }
+
+        // GET api/<Cliente>/5
+        [HttpGet("{id}")]
+        public CursoViewModel Get(Guid id)
+        {
+            return _cursoApplication.Find(id);
+        }
+
+        // POST api/<Cliente>
+        [HttpPost]
+        public void Post([FromBody] CursoViewModel curso)
+        {
+            _cursoApplication.Add(curso);
+        }
+
+        // PUT api/<Cliente>
+        [HttpPut]
+        public void Put([FromBody] CursoViewModel curso)
+        {
+            _cursoApplication.Update(curso);
+        }
+
+        // DELETE api/<Cliente>
+        [HttpDelete]
+        public void Delete([FromBody] CursoViewModel curso)
+        {
+            _cursoApplication.Remove(curso);
+        }
+    }
+}
---
+++ GerenciadorDeCursosPosInfnet/Controllers/DisciplinaController.cs
@@ -0,0 +1,53 @@
+﻿using Cursos.Application.Intefaces;
+using Cursos.Application.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace Cursos.Presentation.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DisciplinaController : ControllerBase
+    {
+        private readonly IDisciplinaApplication _disciplinaApplication;
+        public DisciplinaController(IDisciplinaApplication disciplinaApplication)
+        {
+            _disciplinaApplication = disciplinaApplication;
+        }
+        // GET: api/<Cliente>
+        [HttpGet]
+        public IEnumerable<DisciplinaViewModel> Get()
+        {
+            return _disciplinaApplication.List();
+        }
+
+        // GET api/<Cliente>/5
+        [HttpGet("{id}")]
+        public DisciplinaViewModel Get(Guid id)
+        {
+            return _disciplinaApplication.Find(id);
+        }
+
+        // POST api/<Cliente>
+        [HttpPost]
+        public void Post([FromBody] DisciplinaViewModel disciplina)
+        {
+            _disciplinaApplication.Add(disciplina);
+        }
+
+        // PUT api/<Cliente>
+        [HttpPut]
+        public void Put([FromBody] DisciplinaViewModel disciplina)
+        {
+            _disciplinaApplication.Update(disciplina);
+        }
+
+        // DELETE api/<Cliente>
+        [HttpDelete]
+        public void Delete([FromBody] DisciplinaViewModel disciplina)
+        {
+            _disciplinaApplication.Remove(disciplina);
+        }
+    }
+}
